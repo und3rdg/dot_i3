c941898ee168ae09b29a0e804670f85352d2fa95
und3rdg <und3rdg@gmail.com>
2018-01-21 11:53:39 +0000

    Replace dmenu with rofi

---
+++ rofiConfig.sh
@@ -0,0 +1,2 @@
+rofi -combi-modi window,drun -show combi -color-window '#000000, #000000, #000000' -color-normal '#000000, #b3e774, #000000, #b3e774, #000000' -color-active '#000000, #b3e774, #000000, #b3e774, #000000' -color-urgent '#000000, #b3e774, #000000, #b3e774, #000000'
+
