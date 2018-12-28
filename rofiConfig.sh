844a3f0e17bf3d52a8f5f50ec99220b772610eee
und3rdg <und3rdg@gmail.com>
2018-12-28 16:22:55 +0000

    luke i3 config moved from dot_init repo

--- rofiConfig.sh
+++
@@ -1,8 +0,0 @@
-rofi -combi-modi window,drun \
-  -show combi \
-  -color-window '#000000, #000000, #000000' \
-  -color-normal '#000000, #b3e774, #000000, #b3e77400, #000000' \
-  -color-active '#000000, #b3e774, #000000, #b3e77400, #000000' \
-  -color-urgent '#000000, #b3e774, #000000, #b3e77455, #000000' \
-  -terminal termite
-
